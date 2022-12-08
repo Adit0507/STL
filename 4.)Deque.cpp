71eb9ecac4c15f11659ed7b4634c6a01c357d575
Adit0507 <adi05072002@gmail.com>
2022-12-08 20:28:17 +0530

    Finished Deque

---
+++ 4.)Deque.cpp
@@ -0,0 +1,19 @@
+// Rest Func. same as VECTOR
+
+#include <bits\stdc++.h>
+using namespace std;
+
+void explainDequeue(){
+    deque<int> dq;
+
+    dq.push_back(1);    // {1}
+    dq.emplace_back(2);  // {1,2}
+    dq.push_front(4);   // {4,1,2}
+    dq.emplace_front(3);    // {3,4,1,2}
+
+    dq.pop_back();   // {3,4,1}
+    dq.pop_front();  // {4, 1}
+
+    dq.back();
+    dq.front();
+}
